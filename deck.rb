51b9a4a04c10e4392f8ffab0b94a516149c9fbab
nasajon-chan <alexandrenasajon66@hotmail.com>
2017-12-21 19:40:58 -0200

    Add files via upload

--- deck.rb
+++ deck.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 require 'squib'
-
+version  = 'v0.2'
 Squib::Deck.new(cards: 20, layout: ['layout.yml']) do
   background color: :white
   rect layout: 'cut'
@@ -14,7 +14,7 @@ Squib::Deck.new(cards: 20, layout: ['layout.yml']) do
   #png layout: 
   png file:data['Espiral'].map!{|x| if x!=nil then 'imagens/icones/'+x end}, layout: 'bonus2'
   png file:data['Nome'].map!{|x| 'imagens/animais/'+x +'.png'}, layout: 'art'
-  text str:'v0.1', layout: 'bonus5', font:'Arial 24'
+  text str:version, layout: 'bonus5', font:'Arial 24'
 
   save_pdf file:'animais.pdf',width:'8.3 in',height: '11.7 in', margin: 0
 
@@ -25,8 +25,8 @@ Squib::Deck.new(cards: 29, layout: ['layout.yml']) do
   rect layout: 'cut'
   rect layout: 'safe'
   data = csv file: 'Veganismo - Impactos.csv'
-  text str: data['Nome'].map!{|x| x.force_encoding(::Encoding::UTF_8)}, layout: 'title', font:'Arial 60'
-  text str: data['Tipo'].map!{|x| x.force_encoding(::Encoding::UTF_8)}, layout: 'snark', font:'Arial 50'
+  text str: data['Nome'].map!{|x| x.force_encoding(::Encoding::UTF_8)}, layout: 'name', font:'Arial 60'
+  text str: data['Tipo'].map!{|x| x.force_encoding(::Encoding::UTF_8)}, layout: 'subtype', font:'Arial 50'
   #text str: data['Valor'], layout: 'bonus1', font:'Arial bold 96'
   text str: data['Desc'].map!{|x| x.force_encoding(::Encoding::UTF_8)}, layout: 'description', font:'Arial 24'
 
@@ -34,7 +34,7 @@ Squib::Deck.new(cards: 29, layout: ['layout.yml']) do
   rect layout: 'bonus1'
   rect layout: 'bonus2'
   rect layout: 'bonus3'
-  text str:'v0.1', layout: 'bonus5', font:'Arial 24'
+  text str:version, layout: 'bonus5', font:'Arial 24'
   png file:data['Estrela'].map!{|x| if x!=nil then 'imagens/icones/'+x end}, layout: 'bonus1'
   png file:data['Espiral'].map!{|x| if x!=nil then 'imagens/icones/'+x end}, layout: 'bonus2'
   png file:data['Equip'].map!{|x| if x!=nil then 'imagens/icones/'+x end}, layout: 'bonus3'
