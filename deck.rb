f26635a3bb23cef043d6448f29e0e7b535b4f4be
nasajon-chan <alexandrenasajon66@hotmail.com>
2017-12-21 18:42:33 -0200

    Add files via upload

---
+++ deck.rb
@@ -0,0 +1,47 @@
+# encoding: UTF-8
+require 'squib'
+
+Squib::Deck.new(cards: 20, layout: ['hand.yml','layout.yml']) do
+  background color: :white
+  rect layout: 'cut'
+  rect layout: 'safe'
+  data = csv file: 'Veganismo - Animais.csv'
+  text str: data['Nome'].map!{|x| x.force_encoding(::Encoding::UTF_8)}, layout: 'title', font:'Arial 60'
+  text str: data['Ambiente'].map!{|x| x.force_encoding(::Encoding::UTF_8)}, layout: 'description', font:'Arial 50'
+  text str: data['Valor'], layout: 'bonus1', font:'Arial bold 96'
+  text str: data['Desc'].map!{|x| x.force_encoding(::Encoding::UTF_8)}, layout: 'snark', font:'Arial italic 24'
+
+  #png layout: 
+  png file:data['Espiral'].map!{|x| if x!=nil then 'imagens/icones/'+x end}, layout: 'bonus2'
+  png file:data['Nome'].map!{|x| 'imagens/animais/'+x +'.png'}, layout: 'art'
+  text str:'v0.1', layout: 'bonus5', font:'Arial 24'
+
+  save_pdf file:'animais.pdf',width:'8.3 in',height: '11.7 in', margin: 0
+
+end
+
+Squib::Deck.new(cards: 29, layout: ['hand.yml','layout.yml']) do
+  background color: :white
+  rect layout: 'cut'
+  rect layout: 'safe'
+  data = csv file: 'Veganismo - Impactos.csv'
+  text str: data['Nome'].map!{|x| x.force_encoding(::Encoding::UTF_8)}, layout: 'title', font:'Arial 60'
+  text str: data['Tipo'].map!{|x| x.force_encoding(::Encoding::UTF_8)}, layout: 'snark', font:'Arial 50'
+  #text str: data['Valor'], layout: 'bonus1', font:'Arial bold 96'
+  text str: data['Desc'].map!{|x| x.force_encoding(::Encoding::UTF_8)}, layout: 'description', font:'Arial 24'
+
+  #png layout: 
+  rect layout: 'bonus1'
+  rect layout: 'bonus2'
+  rect layout: 'bonus3'
+  text str:'v0.1', layout: 'bonus5', font:'Arial 24'
+  png file:data['Estrela'].map!{|x| if x!=nil then 'imagens/icones/'+x end}, layout: 'bonus1'
+  png file:data['Espiral'].map!{|x| if x!=nil then 'imagens/icones/'+x end}, layout: 'bonus2'
+  png file:data['Equip'].map!{|x| if x!=nil then 'imagens/icones/'+x end}, layout: 'bonus3'
+  png file:data['Img'].map!{|x| 'imagens/impactos/'+x.force_encoding(::Encoding::UTF_8) +'.png'}, layout: 'art'
+
+
+  save_pdf file:'impactos.pdf',width:'8.3 in',height: '11.7 in', margin: 0
+
+end
+
